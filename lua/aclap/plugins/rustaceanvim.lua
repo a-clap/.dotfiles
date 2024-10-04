cee0e6f8bc223ece877fffdb95d458d7c41ee44f
a-clap <clapinskiadam@gmail.com>
2024-10-04 17:51:38 +0200

    feat(rust): keybindings

--- lua/aclap/plugins/rustaceanvim.lua
+++ lua/aclap/plugins/rustaceanvim.lua
@@ -10,6 +10,14 @@ return {
     -- p  vim.keymap.set("n", "K", function() vim.cmd.RustLsp { "hover", "actions" } end, { buffer = bufnr })
     keymap.set("n", "K", function()
       vim.cmd.RustLsp { "hover", "actions" }
+      vim.cmd.RustLsp { "hover", "actions" }
     end, { desc = "hover action" })
+
+    keymap.set("n", "T", function()
+      vim.cmd.RustLsp { "testables" }
+    end, { desc = "list testables" })
+    keymap.set("n", "R", function()
+      vim.cmd.RustLsp { "runnables" }
+    end, { desc = "list runnables" })
   end,
 }
