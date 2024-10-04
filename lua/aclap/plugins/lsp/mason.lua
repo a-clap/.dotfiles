50a636a0b97ee5ed26b8dfa71c7279c91b5c092e
a-clap <clapinskiadam@gmail.com>
2024-10-04 17:51:43 +0200

    feat(rust): bacon

--- lua/aclap/plugins/lsp/mason.lua
+++ lua/aclap/plugins/lsp/mason.lua
@@ -42,6 +42,7 @@ return {
       mason_tool_installer.setup {
         ensure_installed = {
           "asmfmt",
+          "bacon",
           "stylua", -- lua formatter
           "isort", -- python formatter
           "black", -- python formatter
