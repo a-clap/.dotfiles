930262dca21e691fe1ab71a99fc94ffa41daa712
a-clap <clapinskiadam@gmail.com>
2024-10-26 07:09:41 +0200

    feat(nvim): add doge

---
+++ .config/nvim/lua/aclap/plugins/docs.lua
@@ -0,0 +1,19 @@
+return {
+  "kkoomen/vim-doge",
+  config = function()
+    vim.keymap.set("n", "<leader>dc", "<cmd>DogeGenerate<CR>", { desc = "Generate docs" })
+    -- Interactive mode comment todo-jumping
+    vim.keymap.set("n", "<TAB>", "<Plug>(doge-comment-jump-forward)")
+    vim.keymap.set("n", "<S-TAB>", "<Plug>(doge-comment-jump-backward)")
+    vim.keymap.set("i", "<TAB>", "<Plug>(doge-comment-jump-forward)")
+    vim.keymap.set("i", "<S-TAB>", "<Plug>(doge-comment-jump-backward)")
+    vim.keymap.set("x", "<TAB>", "<Plug>(doge-comment-jump-forward)")
+    vim.keymap.set("x", "<S-TAB>", "<Plug>(doge-comment-jump-backward)")
+    vim.g.doge_doc_standard_cpp = "doxygen_cpp_comment_slash"
+    vim.g.doge_doc_standard_c = "doxygen_cpp_comment_slash"
+    -- vim.g.doge_doxygen_settings = {  ("char"): "\\"}
+    vim.g.doge_doxygen_settings = {
+      char = "\\",
+    }
+  end,
+}
