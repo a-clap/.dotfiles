f0ac2d8c8da6d43bfeceee19863358352bdd0ea4
a-clap <clapinskiadam@gmail.com>
2024-12-11 09:49:37 +0100

    feat(nvim): use lazygit from snacks

--- .config/nvim/lua/aclap/plugins/snacks.lua
+++ .config/nvim/lua/aclap/plugins/snacks.lua
@@ -11,5 +11,17 @@ return {
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
+    lazygit = { enabled = true },
+    terminal = { enabled = true },
+  },
+-- stylua: ignore
+  keys = {
+    -- lazygit
+    { "<leader>gb", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+    -- terminal
+    { "<C-_>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
   },
 }
