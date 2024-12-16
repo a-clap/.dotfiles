c81b39b32ff861a72d24ff52fd01635e39f8a8ea
a-clap <clapinskiadam@gmail.com>
2024-12-16 06:16:36 +0100

    feat(nvim): snacks

--- .config/nvim/lua/aclap/plugins/snacks.lua
+++ .config/nvim/lua/aclap/plugins/snacks.lua
@@ -23,5 +23,7 @@ return {
     { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
     -- terminal
     { "<C-_>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
   },
 }
