3311e476f1ab9d867458ac092406045dedd5b97c
a-clap <clapinskiadam@gmail.com>
2025-02-27 12:25:25 +0100

    feat(nvim): better gruvbox theme

--- .config/nvim/lua/aclap/plugins/gruvbox.lua
+++ .config/nvim/lua/aclap/plugins/gruvbox.lua
@@ -35,6 +35,7 @@ return {
   {
     {
       "f4z3r/gruvbox-material.nvim",
+      enabled = false,
       config = function()
         require("gruvbox-material").setup {
           -- contrast = "low",
@@ -44,6 +45,26 @@ return {
           vim.cmd "colorscheme gruvbox-material",
         }
       end,
+    },
+  },
+  {
+    {
+      {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 1000,
+        config = function()
+          -- Optionally configure and load the colorscheme
+          -- directly inside the plugin declaration.
+          --gruvbox_material_background
+          vim.g.gruvbox_material_background = "hard"
+          vim.g.gruvbox_material_foreground = "mix"
+          vim.g.gruvbox_material_enable_italic = false
+          vim.g.gruvbox_material_enable_bold = 1
+
+          vim.cmd.colorscheme "gruvbox-material"
+        end,
+      },
     },
   },
 }
