a2ef11c72f3619ab39cc0c6ccc22ca73e518ff3d
a-clap <clapinskiadam@gmail.com>
2025-03-05 21:18:14 +0100

    feat(nvim): luasnip

--- .config/nvim/lua/aclap/plugins/nvim-cmp.lua
+++ .config/nvim/lua/aclap/plugins/nvim-cmp.lua
@@ -43,6 +43,21 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm { select = false },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i" }),
       },
       -- sources for autocompletion
       sources = cmp.config.sources {
