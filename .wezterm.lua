50f9cac0fce9d8665da4e44822ed71be3a5e99c1
a-clap <clapinskiadam@gmail.com>
2025-08-31 14:10:27 +0200

    feat(wezterm): testing new config

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,16 +5,31 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config = {
+  color_scheme = "Gruvbox Dark (Gogh)",
 
-config.font = wezterm.font "MesloLGS Nerd Font Mono"
-config.font_size = 12
+  -- font = wezterm.font "MesloLGS Nerd Font Mono",
+  font = wezterm.font "JetBrainsMono Nerd Font",
+  font_size = 14.5,
 
-config.enable_tab_bar = false
+  enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  default_cursor_style = "SteadyBlock",
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 15,
+    bottom = 0,
+  },
 
--- config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.95
-config.macos_window_background_blur = 10
+  -- config.window_decorations = "RESIZE"
+  window_background_opacity = 0.95,
+  macos_window_background_blur = 20,
+  max_fps = 120,
+
+  -- Removes the macos bar at the top with the 3 buttons
+  window_decorations = "RESIZE",
+}
 
 -- and finally, return the configuration to wezterm
 return config
