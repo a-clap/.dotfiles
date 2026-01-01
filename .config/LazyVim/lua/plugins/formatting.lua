93917be172d7d0892f15ec86cdbad3e7761f691d
a-clap <clapinskiadam@gmail.com>
2026-01-01 18:03:18 +0100

    feat(LazyVim): formatting

---
+++ .config/LazyVim/lua/plugins/formatting.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        asm = { "asmfmt" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        go = { "gofumpt", "golines", "goimports-reviser" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        json = { "prettier" },
+        lua = { "stylua" },
+        markdown = { "prettier" },
+        proto = { "buf" },
+        python = { "ruff_format" },
+        rust = { "rustfmt" },
+        sh = { "shfmt" },
+        toml = { "taplo" },
+        xml = { "xmlformatter" },
+        yaml = { "yamlfmt" },
+      },
+
+      formatters = {
+        yamlfmt = {
+          prepend_args = { "-formatter", "retain_line_breaks_single=true" },
+        },
+        xmlformat = {
+          prepend_args = { "--blanks", "--selfclose" },
+        },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "asmfmt",
+        "black",
+        "buf",
+        "clang-format",
+        "gofumpt",
+        "goimports-reviser",
+        "golines",
+        "isort",
+        "prettier",
+        "rustfmt",
+        "shfmt",
+        "stylua",
+        "taplo",
+        "xmlformatter",
+        "yamlfmt",
+      },
+    },
+  },
+}
