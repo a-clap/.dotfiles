6a8c8b63b87a11f64a8b8465e3d41ceda11f2f14
a-clap <clapinskiadam@gmail.com>
2026-09-03 20:51:56 +0200

    feat(gruvbox): update

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -11,17 +11,16 @@ return {
       "sainnhe/gruvbox-material",
       lazy = false,
       priority = 1000,
-      config = function()
-        -- Optionally configure and load the colorscheme
-        -- directly inside the plugin declaration.
-        --gruvbox_material_background
+      init = function()
+        vim.opt.background = "dark"
+
         vim.g.gruvbox_material_background = "hard"
         vim.g.gruvbox_material_foreground = "mix"
+        vim.g.gruvbox_material_ui_contrast = "high"
+
         vim.g.gruvbox_material_enable_italic = false
         vim.g.gruvbox_material_enable_bold = 1
         vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-
-        -- vim.cmd.colorscheme("gruvbox-material")
       end,
     },
   },
