6df0f1958d2b794485fb823ec908a78a6bab62ad
a-clap <clapinskiadam@gmail.com>
2026-09-03 21:42:54 +0200

    Revert "feat(rust): wip"
    This reverts commit aa5e52061274d9dc03677c02d2f49b8d6ee57b14.

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -14,47 +14,4 @@ return {
       },
     },
   },
-  {
-    "mrcjkb/rustaceanvim",
-    keys = {
-      {
-        "<leader>ce",
-        function()
-          Snacks.terminal("cargo run", {
-            cwd = LazyVim.root(),
-            auto_close = false,
-          })
-        end,
-        desc = "Cargo Run",
-      },
-      {
-        "<leader>cb",
-        function()
-          Snacks.terminal("cargo build", {
-            cwd = LazyVim.root(),
-            auto_close = false,
-          })
-        end,
-        desc = "Cargo Build",
-      },
-      -- {
-      --   "<leader>Rc",
-      --   function()
-      --     Snacks.terminal("cargo check", {
-      --       cwd = LazyVim.root(),
-      --     })
-      --   end,
-      --   desc = "Cargo Check",
-      -- },
-      -- {
-      --   "<leader>Rl",
-      --   function()
-      --     Snacks.terminal("cargo clippy", {
-      --       cwd = LazyVim.root(),
-      --     })
-      --   end,
-      --   desc = "Cargo Clippy",
-      -- },
-    },
-  },
 }
